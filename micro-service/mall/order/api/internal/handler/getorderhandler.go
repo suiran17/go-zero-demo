76189c259382fd3d02fa83620cd58a6775e9af28
zhangpan <2223410846@qq.com>
2023-03-13 12:33:44 +0800

    add go-zero demo  单体服务 + 微服务

---
+++ micro-service/mall/order/api/internal/handler/getorderhandler.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"go-zero-demo/mall/order/api/internal/logic"
+	"go-zero-demo/mall/order/api/internal/svc"
+	"go-zero-demo/mall/order/api/internal/types"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func getOrderHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.OrderReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewGetOrderLogic(r.Context(), svcCtx)
+		resp, err := l.GetOrder(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
