521f15609786cfa2e1bae4a800d2f7a0aa01904a
zp <>
2025-06-21 21:28:57 +0800

    init

---
+++ mall/order/api/internal/logic/getorderlogic.go
@@ -0,0 +1,48 @@
+package logic
+
+import (
+	"context"
+	"errors"
+
+	"go-zero-demo/mall/order/api/internal/svc"
+	"go-zero-demo/mall/order/api/internal/types"
+
+	"go-zero-demo/mall/user/rpc/types/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetOrderLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGetOrderLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetOrderLogic {
+	return &GetOrderLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GetOrderLogic) GetOrder(req *types.OrderReq) (resp *types.OrderReply, err error) {
+	// todo: add your logic here and delete this line
+
+	// return
+
+	user, err := l.svcCtx.UserRpc.GetUser(l.ctx, &user.IdRequest{Id: "1"})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if user.Name != "test" {
+		return nil, errors.New("user not found")
+	}
+
+	return &types.OrderReply{
+		Id:   req.Id,
+		Name: "test order",
+	}, nil
+}
